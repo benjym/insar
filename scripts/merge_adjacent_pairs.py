4fe4a30845458f15994c37a145524514937d88c5
Benjy Marks <benjy.marks@sydney.edu.au>
2021-09-17 18:23:29 +1000

    updated merge script

--- scripts/merge_adjacent_pairs.py
+++ scripts/merge_adjacent_pairs.py
@@ -23,16 +23,10 @@ from shutil import copyfile
 # f:          Swath Number: 1, 2, 3, or Full (F)
 # ssss:       Product ID
 
-# files = []
-# for root, dir, filelist in os.walk('/home/work/data/hyp3/'):
-#     for file in filelist:
-#         # if 'unw_phase.tif' in file or 'corr.tif' in file or 'inc_map.tif' in file or 'dem.tif' in file:
-#         if file[-8:] == 'corr.tif' or file[-13:] == 'unw_phase.tif' or file[-11:] == 'inc_map.tif' or file[-7:] == 'dem.tif':
-#             fullpath = os.path.join(root, file) # Get the full path to the file
-#             files.append(fullpath)
 root_dir = '/home/work/data/hyp3-raw/'
 out_dir = '/home/work/data/hyp3/'
-merge_files = ['_unw_phase.tif', '_corr.tif', '_lv_theta.tif', '_water_mask.tif', '_dem.tif']
+# merge_files = ['unw_phase.tif', 'corr.tif', 'lv_theta.tif', 'water_mask.tif', 'dem.tif']
+merge_files = ['unw_phase.tif', 'corr.tif', 'inc_map.tif', 'dem.tif']
 
 if not os.path.exists(out_dir): os.mkdir(out_dir)
 folders = glob.glob(root_dir + '*/')
@@ -40,24 +34,24 @@ folders.sort()
 prev_two_date_string = ''
 prev_foldername = ''
 
-for folder in folders[:3]:
+for folder in folders:
     foldername = folder[len(root_dir):]
     date_a = foldername[5:13]
     date_b = foldername[21:29]
     two_date_string = date_a+date_b
+    # print(two_date_string)
     if two_date_string == prev_two_date_string:
+        print(f'Found two matching folders for dates {date_a} and {date_b}')
         for f in merge_files:
             if not os.path.exists(out_dir + foldername): os.mkdir(out_dir + foldername)
-
+            print(root_dir + prev_foldername + prev_foldername[:-1] + '*' + f)
             file_a = glob.glob(root_dir + prev_foldername + prev_foldername[:-1] + '*' + f)[0]
             file_b = glob.glob(root_dir + foldername + foldername[:-1] + '*' + f)[0]
-            print(file_a,file_b)
-            command = "gdal_merge.py -o " + out_dir + foldername + f + " -of gtiff " + file_a + file_b
+            print(f'Merging {file_a} and {file_b} to make {out_dir + foldername + f}')
+            command = "gdal_merge.py -o " + out_dir + foldername + f + " -of gtiff " + file_a + " " + file_b
             print(os.popen(command).read())
 
             copyfile(root_dir + foldername + foldername[:-1] + ".txt", out_dir + foldername + foldername[:-1] + ".txt")
 
     prev_two_date_string = two_date_string
     prev_foldername = foldername
-command = "gdal_merge.py -o /Users/olearyd/Git/data/TEAK_Aspect_Tiles/TEAK_Aspect_Mosaic.tif -of gtiff " + files_string
-print(os.popen(command).read())
